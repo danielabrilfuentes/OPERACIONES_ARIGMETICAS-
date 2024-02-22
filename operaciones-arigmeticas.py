34c59e342a14909679bed316095676b5c46c3611
sistemas <luisabril@colegioguanenta.edu.co>
2024-02-22 17:22:24 -0500

    se agrego el codigo python del programa

---
+++ operaciones-arigmeticas.py
@@ -0,0 +1,16 @@
+# input
+x = int (input("digite elvalor de x:"))
+y = int (input("digite el valor de y:"))
+
+# processing
+s=x+y
+r=x-y
+m=x*y
+d=x/y
+de=x//y
+mod=x%y
+p=x**y
+
+# output 
+print ("resultados")
+print ("la suma de"+str(x)+"+"+str(y)+"es"+str(s)+"+"+str(r)+"+"+str(m)+"+"+str(d)+"+"+str(de)+"+"+str(mod)+"+"+str(p))
